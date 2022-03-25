3501a13106b95ce3400d12ecdaaab10e2df788d4
topgun0209 <topgun0209@naver.com>
2022-03-26 04:57:41 +0900

    Update PlayerInfoImpl.java

--- src/main/java/com/gbf/gbf_ff/service/PlayerInfoImpl.java
+++ src/main/java/com/gbf/gbf_ff/service/PlayerInfoImpl.java
@@ -83,8 +83,8 @@ public class PlayerInfoImpl implements PlayerInfo {
 				System.setProperty("webdriver.chrome.driver", "src/main/resources/static/chromedriver_win32/chromedriver.exe");
 			}
 			ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.addArguments("headless");
-			chromeOptions.addArguments("disable-gpu");
+//			chromeOptions.addArguments("headless");
+//			chromeOptions.addArguments("disable-gpu");
 			chromeOptions.addArguments("no-sandbox");
 
 			String binaryLoc=System.getenv("GOOGLE_CHROME_BIN");
