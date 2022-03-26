d63fcd1f25c02eaa7e7def3ae7e529ea9e3f0251
topgun0209 <topgun0209@naver.com>
2022-03-26 17:29:27 +0900

    Update GBFResourceImpl.java

--- src/main/java/com/gbf/gbf_ff/service/GBFResourceImpl.java
+++ src/main/java/com/gbf/gbf_ff/service/GBFResourceImpl.java
@@ -161,6 +161,7 @@ public class GBFResourceImpl implements GBFResource {
 		File file = new File(imgSaveUrl+playerDto.getId()+"/merged.jpg");
 		if(!file.exists())file.mkdirs();
 		System.out.println(file.getPath());
+		System.out.println(baseImg);
 
 		ImageIO.write(baseImg, "jpg", file);
 
